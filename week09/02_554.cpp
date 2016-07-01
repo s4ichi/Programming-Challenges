efc85bb87951f03928b947d60ce25c59f8887246
everysick <s.wakeup31@gmail.com>
2016-07-01 14:21:35 +0900

    Solve week09/02_554.cpp

--- week09/02_554.cpp
+++ week09/02_554.cpp
@@ -1,60 +1,66 @@
 #include <iostream>
+#include <set>
+#include <vector>
 #include <string>
-#include <numeric>
 
 using namespace std;
 
-struct Trie {
-	int value;
-	Trie *next[0x100];
-
-	Trie() {
-		fill(next, next+0x100, (Trie*)0);
-	}
-};
-
-void add(string s, Trie *r) {
-	for (int i = 0; s.size(); ++i) {
-		char c = s[i];
-		if (!r->next[c]) r->next[c] = new Trie;
-		r = r->next[c];
-	}
-
-	return r;
-}
-
-Trie *find(string s, Trie *r) {
-	for (int i = 0; s.size(); ++i) {
-		char c = s[i];
-		if (!r->next[c]) return false;
-		r = r->next[c];
-	}
-
-	return true;
-}
 
 int main() {
-	Trie t;
+	set<string> dict;
+	string s, best;
 
-	int m = 0;
-	string s;
 	while (cin >> s, s[0] != '#') {
-		add(s, t);
-		m = max(m, s.size());
+		dict.insert(s);
 	}
 
-	string cy = "";
-	while (cin >> s) {
-		cy += s;
+	cin.ignore();
+	getline(cin, s);
+	int n = s.size(), ma = 0;
+
+	for (int i=0; i<n; i++) {
+		s[i] = (s[i] == ' ')?'@':s[i];
 	}
 
-	for (int k=1; k<26; k++) {
-		for (int i=0; i<cy.size()-m; i++) {
-			s = "";
-			for (int j=0; j<m; j++) {
-				s += (cy[i+j] + k) % 27;
+	for (int k=0; k<27; k++){
+		string tmp = s;
+
+		for (int i=0; i<n; i++) {
+			tmp[i] = '@' + (((tmp[i] - '@') + k) % 27);
+		}
+
+		int cnt = 0;
+		for (int i=0; i<n; i++){
+			int j = i;
+			while (tmp[j] != '@' && j<n) j++;
+
+			if (dict.find(tmp.substr(i, j-i)) != dict.end()) {
+				cnt++;
 			}
 		}
+
+		if (ma < cnt){
+			ma = cnt;
+			best = tmp;
+		}
+	}
+
+	vector<string> term;
+	for (int i=0; i<n; i++) {
+		int j = i + 1;
+		while (best[j] != '@' && j<n) j++;
+		term.push_back(best.substr(i, j-i));
+		i = j;
+	}
+
+	for (int i=0; i<term.size(); i++) {
+		string buf = term[i];
+		while ((buf+term[i+1]).length() < 60 && i+1<term.size()) {
+			buf += " " + term[i+1];
+			i++;
+		}
+
+		cout << buf << endl;;
 	}
 
 	return 0;
